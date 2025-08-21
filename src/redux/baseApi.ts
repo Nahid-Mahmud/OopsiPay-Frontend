09cc964ded7ec92141c25f6e4e49dbb6c049a623
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-21 13:09:05 +0600

    feat: integrate Redux Toolkit with Axios for API management
    - Added axios instance with interceptors for handling token expiration and refreshing.
    - Created a base API using Redux Toolkit's createApi for managing API endpoints.
    - Implemented authentication endpoints including register, login, send OTP, verify OTP, user info retrieval, and logout.
    - Established custom hooks for dispatching actions and selecting state from the Redux store.
    - Configured the Redux store to include the base API reducer and middleware.
    - Introduced environment configuration for base URL.
    - Added Axios and Redux Toolkit dependencies to package.json.

---
+++ src/redux/baseApi.ts
@@ -0,0 +1,8 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import axiosBaseQuery from "./axiosBaseQuery";
+
+export const baseApi = createApi({
+  baseQuery: axiosBaseQuery(),
+  endpoints: () => ({}),
+  tagTypes: ["User"],
+});
---
+++ src/redux/features/auth/auth.api.ts
@@ -0,0 +1,76 @@
+import { baseApi } from "@/redux/baseApi";
+
+type TOtp = {
+  email: string;
+};
+
+type TResponse<T> = {
+  success: boolean;
+  message: string;
+  data: T;
+  statusCode: number;
+};
+
+type TVerifyOtp = {
+  email: string;
+  otp: string;
+};
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "/user/register",
+        method: "POST",
+        data: data,
+      }),
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/auth/login",
+        method: "POST",
+        data: data,
+      }),
+    }),
+
+    sendOtp: builder.mutation<TResponse<null>, TOtp>({
+      query: (data) => ({
+        url: "/otp/send",
+        method: "POST",
+        data: data,
+      }),
+    }),
+
+    verifyOtp: builder.mutation<TResponse<null>, TVerifyOtp>({
+      query: (data) => ({
+        url: "/otp/verify",
+        method: "POST",
+        data: data,
+      }),
+    }),
+
+    userInfo: builder.query<TResponse<null>, void>({
+      query: () => ({
+        url: "/user/me",
+        method: "GET",
+      }),
+      providesTags: ["User"],
+    }),
+
+    logout: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+  useSendOtpMutation,
+  useVerifyOtpMutation,
+  useUserInfoQuery,
+  useLogoutMutation,
+} = authApi;
