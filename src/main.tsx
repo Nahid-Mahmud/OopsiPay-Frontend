e336187f38aba92f8c6f942d44c1ef11c0fdc9d1
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-22 20:57:43 +0600

    feat: wrap RouterProvider and Toaster in Redux Provider for state management

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import { RouterProvider } from "react-router";
 import { Toaster } from "sonner";
 import "./index.css";
 import { router } from "./routes/index.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <Toaster richColors duration={3000} closeButton position="top-right" />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <Toaster richColors duration={3000} closeButton position="top-right" />
+    </Provider>
   </StrictMode>
 );
