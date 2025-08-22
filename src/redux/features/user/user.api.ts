a7ccf19bae8b77580433e566b25974ebb633a25f
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-22 22:25:27 +0600

    feat: implement user API with register and userInfo endpoints

---
+++ src/redux/features/user/user.api.ts
@@ -0,0 +1,25 @@
+import { baseApi } from "@/redux/baseApi";
+import type { TResponse } from "@/types/response.types";
+import type { IUser } from "@/types/user.types";
+
+export const userApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation<TResponse<IUser>, Partial<IUser>>({
+      query: (data) => ({
+        url: "/user/create",
+        method: "POST",
+        data: data,
+      }),
+    }),
+
+    userInfo: builder.query<TResponse<null>, void>({
+      query: () => ({
+        url: "/user/me",
+        method: "GET",
+      }),
+      providesTags: ["User"],
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useUserInfoQuery } = userApi;
