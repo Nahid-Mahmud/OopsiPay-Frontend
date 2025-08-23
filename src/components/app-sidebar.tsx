0abaf45652c7a9fa58730827f90784febeff049e
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-23 18:57:33 +0600

    feat: implement AppSidebar component for admin panel navigation

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,72 @@
+import { ChevronRight, File, Folder } from "lucide-react";
+import * as React from "react";
+
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { AdminSidebarItems } from "@/routes/AdminSidebarItems";
+import type { ISidebarItem } from "@/types";
+import { Link } from "react-router";
+
+console.log(AdminSidebarItems);
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Admin Panel</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {AdminSidebarItems.map((item, index) => (
+                <SidebarItem key={index} item={item} />
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
+
+function SidebarItem({ item }: { item: ISidebarItem }) {
+  return (
+    <SidebarMenuItem>
+      <Collapsible
+        className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+        defaultOpen={false}
+      >
+        <CollapsibleTrigger asChild>
+          <SidebarMenuButton>
+            <ChevronRight className="transition-transform" />
+            <Folder />
+            {item.title}
+          </SidebarMenuButton>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <SidebarMenuSub>
+            {item.items.map((subItem, index) => (
+              <SidebarMenuButton key={index} isActive={false} className="data-[active=true]:bg-transparent">
+                <Link to={subItem.url} className="inline-flex items-center gap-2">
+                  {subItem.icon ? subItem.icon : <File />}
+                  {subItem.title}
+                </Link>
+              </SidebarMenuButton>
+            ))}
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </Collapsible>
+    </SidebarMenuItem>
+  );
+}
