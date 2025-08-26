47661ceffb1efa47bb57697881030768ac3645d1
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-08-27 00:36:48 +0600

    refactor: replace hardcoded tagTypes with imported tagTypes from tagTypes module

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -1,8 +1,9 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import axiosBaseQuery from "./axiosBaseQuery";
+import { tagTypes } from "./tagTypes";
 
 export const baseApi = createApi({
   baseQuery: axiosBaseQuery(),
   endpoints: () => ({}),
-  tagTypes: ["User", "Transaction", "Wallet"],
+  tagTypes: tagTypes,
 });
